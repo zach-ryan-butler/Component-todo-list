2d7c92950accddd4a3f4706437c7642121c8009a
Zach Butler <zach.standley93@gmail.com>
2019-05-22 14:56:15 -0700

    added in past modules as helper functions

---
+++ src/components/Component.js
@@ -0,0 +1,31 @@
+import htmlToDom from '../html-to-DOM.js';
+
+class Component {
+
+    constructor(props) {
+        this.props = props;
+    }
+
+    render() {
+        return this.renderDom();
+    }
+
+    renderDom() {
+        const html = this.renderTemplate();
+        const dom = htmlToDom(html);
+        this.rootElement = dom;
+        return dom;
+    }
+
+    update(props) {
+        // update the props:
+        Object.keys(props).forEach(key => {
+            this.props[key] = props[key];
+        });
+        const oldRoot = this.rootElement;
+        const newDOM = this.render();
+        oldRoot.replaceWith(newDOM);
+    }
+}
+
+export default Component;
